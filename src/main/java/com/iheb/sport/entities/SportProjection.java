acc6ad62e3578779bbb7026ded29b1269045a3c7
iheb-2023-web <deruichiheb3@gmail.com>
2024-10-15 18:29:58 +0100

    Ajout du projet userMicroservice

--- src/main/java/com/iheb/sport/entities/SportProjection.java
+++
@@ -1,7 +0,0 @@
-package com.iheb.sport.entities;
-
-import org.springframework.data.rest.core.config.Projection;
-@Projection(name = "nomSport", types = { sport.class })
-public interface SportProjection {
-    public String getNomSport();
-}
